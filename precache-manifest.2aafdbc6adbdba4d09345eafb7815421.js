086961bf23491fc6599cb86e8614c778b407b096
Morema <25913934+Mo-rema07@users.noreply.github.com>
2020-01-13 01:45:02 +0400

    Updates

---
+++ precache-manifest.2aafdbc6adbdba4d09345eafb7815421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ef740f90316c5659de76e8b6d314bb",
+    "url": "/dou_dev/index.html"
+  },
+  {
+    "revision": "084d76a0e2ec589498a0",
+    "url": "/dou_dev/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eb5dd7f5ef90210229b5",
+    "url": "/dou_dev/static/js/2.2e9dbcc7.chunk.js"
+  },
+  {
+    "revision": "6dbd9e7d4b65b27382d6cb0964c77a31",
+    "url": "/dou_dev/static/js/2.2e9dbcc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "084d76a0e2ec589498a0",
+    "url": "/dou_dev/static/js/main.bbf26e5d.chunk.js"
+  },
+  {
+    "revision": "04e70ccc07f88c374b81",
+    "url": "/dou_dev/static/js/runtime-main.85aa6c22.js"
+  }
+]);
